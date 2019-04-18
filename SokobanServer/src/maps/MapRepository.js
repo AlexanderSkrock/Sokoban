d4002af9792ba2e36e15c5142b3dbdbba8bb665d
Alexander Skrock <alexanderskrock@gmx.de>
2019-04-18 23:27:00 +0200

    #2 Korrektur beim Lesen der maps.json

--- SokobanServer/src/maps/MapRepository.js
+++ SokobanServer/src/maps/MapRepository.js
@@ -2,9 +2,12 @@ export default class MapRepository {
   static MAP_PATH = "data/maps/maps.json";
 
   static getMaps(callback) {
-    fs.readFile(MapRepository.MAP_PATH, maps => {
-      const result = maps || [];
-      callback(result);
+    fs.readFile(MapRepository.MAP_PATH, (error, data) => {
+      let maps = [];
+      if(!error && data) {
+        maps = JSON.parse(data)
+      }
+      callback(maps);
     });
   }
 
@@ -16,22 +19,29 @@ export default class MapRepository {
   }
 
   static putMap(map, callback)  {
-    MapRepository.deleteMap(map.id, () => {
+    MapRepository.deleteMap(map, () => {
       this.getMaps(maps => {
+        if(!map.id) {
+          map.id = MapRepository.generateId();
+        }
         const alteredMaps = [ ...maps, map ];
         MapRepository.saveMaps(alteredMaps, callback);
       })
     });
   }
 
-  static deleteMap(id, callback) {
+  static deleteMap(mapToDelete, callback) {
     MapRepository.getMaps(maps => {
-      const filteredMaps = maps.filter(map => map.id === id);
+      const filteredMaps = maps.filter(map => map.id === mapToDelete.id);
       MapRepository.saveMaps(filteredMaps, callback);
     });
   }
 
   static saveMaps(maps, callback) {
     fs.writeFile(MapRepository.MAP_PATH, JSON.stringify(maps), callback)
+  }
+
+  static generateId() {
+    return 0;
   }
 }
