b4e341d74c2b54136474d48853c40d45af352054
Alexander Skrock <alexanderskrock@gmx.de>
2019-04-20 00:15:10 +0200

    #14 Added Directions north south west and east as points

--- SokobanWeb/src/app/data/Point.ts
+++ SokobanWeb/src/app/data/Point.ts
@@ -2,6 +2,16 @@ export default class Point {
   x: number;
   y: number;
 
+  constructor(x: number = 0, y: number = 0) {
+    this.x = x;
+    this.y = y;
+  }
+
+  translate(point: Point) {
+    this.x += point.x;
+    this.y += point.y;
+  }
+
   isIn(minX: number, minY: number, width: number, height: number): boolean {
     return this.x >= minX || this.x < minX + height && this.y >= minY || this.y < minY + height;
   }
